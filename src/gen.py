b1f54deba885385d6f52f1f12d440ee9df4cc8b0
Bartosz Golis <108265944+ProjectsDev01@users.noreply.github.com>
2025-01-08 19:21:21 +0100

    Delete src directory

--- src/gen.py
+++
@@ -1,9 +0,0 @@
-def generate_image(text, generator, text_encoder, z_dim=100):
-
-    text_emb = text_encoder.encode(text)  
-    z = torch.randn(1, z_dim).to(device)
-    
-    with torch.no_grad():
-        generated_img = generator(z, text_emb)
-    
-    return generated_img
--- src/gui.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-def generate_image_from_input(text):
-    img = generate_image(text, generator, text_encoder)
-    return img
-
-interface = gr.Interface(fn=generate_image_from_input, inputs="text", outputs="image")
-interface.launch()
